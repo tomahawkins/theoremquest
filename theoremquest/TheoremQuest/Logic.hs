4fdf459e846ebd93e6e3f200fc472671cfa8d64e
Tom Hawkins <tomahawkins@gmail.com>
2011-02-25 23:07:25 -0600

    type checking

--- theoremquest/TheoremQuest/Logic.hs
+++ theoremquest/TheoremQuest/Logic.hs
@@ -3,22 +3,26 @@ module TheoremQuest.Logic
   , Proposition
   , Variable      (..)
   , Theorem
-  , TypeVariable  (..)
-  , TypeTerm      (..)
+  , Type          (..)
   , Inference     (..)
   , Axiom         (..)
+  , TypeOf        (..)
   , (=.)
   , assumptions
   , conclusion
   , inference
+  , wellTyped
+  , freeIn
+  , freeVariables
   ) where
 
 import Data.List
 
-infix 4 =.
+infix  4 =.
+infixr 1 :->
 
 data Term
-  = Const String TypeTerm
+  = Const String Type
   | Var   Variable
   | Abs   Variable Term
   | Comb  Term Term
@@ -27,8 +31,11 @@ data Term
 -- | A boolean term.
 type Proposition = Term
 
-data Variable = Variable String TypeTerm
-  deriving (Show, Read, Eq)
+data Variable = Variable String Type
+  deriving (Show, Read)
+
+instance Eq Variable where
+  Variable a _ == Variable b _ = a == b
 
 data Theorem = Theorem [Proposition] Proposition
 
@@ -40,34 +47,51 @@ assumptions (Theorem a _) = a
 conclusion :: Theorem -> Proposition
 conclusion (Theorem _ a) = a
 
-data TypeVariable
-  = TypeVariable
-  deriving (Show, Read, Eq)
+data Type
+  = Bool
+  | Type :-> Type
+  | NotWellTyped
+  deriving (Show, Read)
 
-data TypeTerm
-  = TypeTerm
-  deriving (Show, Read, Eq)
+instance Eq Type where
+  Bool == Bool = True
+  (a :-> b) == (x :-> y) = a == x && b == y
+  _ == _ = False
+
 
 data Inference a
-  = REFL                Term
-  | TRANS               a a
-  | MK_COMB             a a
-  | ABS                 Term a
-  | BETA                Term
-  | ASSUME              Term
-  | EQ_MP               a a
-  | DEDUCT_ANTISYM_RULE a a
-  | INST                a [(Variable, Term)]
-  | INST_TYPE           a [(TypeVariable, TypeTerm)]
-  | AXIOM               Axiom
+  = REFL           Term
+  | TRANS          a a
+  | MK_COMB        a a
+  | ABS            Term a
+  | BETA           Term
+  | ASSUME         Term
+  | EQ_MP          a a
+  | DEDUCT_ANTISYM a a
+  | INST           [(Variable, Term)] a
+  | INST_TYPE      [(Type, Type)] a
+  | AXIOM          Axiom
   deriving (Show, Read)
 
 data Axiom
   = Axiom
   deriving (Show, Read)
 
+class    TypeOf a        where typeOf :: a -> Type
+instance TypeOf Type     where typeOf = id
+instance TypeOf Variable where typeOf (Variable _ t) = t
+instance TypeOf Term where
+  typeOf a = case a of
+    Const _ t -> t
+    Var v -> typeOf v
+    Comb f _ -> case typeOf f of
+      _ :-> a -> a
+      _ -> NotWellTyped
+    Abs (Variable _ t) b -> t :-> typeOf b
+
 (=.) :: Term -> Term -> Term
-a =. b = Comb (Comb (Const "=" TypeTerm {-"a -> a -> bool"-}) a) b
+a =. b = Comb (Comb (Const "=" $ typeOf a :-> typeOf a :-> Bool) a) b
+-- XXX Need to better understand mk_eq.
 
 -- | Creates a 'Theorem' from an 'Inference' rule application.
 inference :: Inference Theorem -> Maybe Theorem
@@ -79,8 +103,8 @@ inference rule = case rule of
   BETA a@(Comb (Abs x1 t)(Var x2)) | x1 == x2 -> validateTheorem [] $ a =. t
   ASSUME p -> validateTheorem [p] p
   EQ_MP (Theorem a1 (Comb (Comb (Const "=" _) a2) a3)) (Theorem b1 b2) | a2 == b2 -> validateTheorem (union a1 b1) a3
-  DEDUCT_ANTISYM_RULE (Theorem a1 a2) (Theorem b1 b2) -> validateTheorem (union (delete b2 a1) (delete a2 b1)) $ a2 =. b2
-  INST (Theorem assumes prop) subs -> validateTheorem (map (replace subs) assumes) (replace subs prop)
+  DEDUCT_ANTISYM (Theorem a1 a2) (Theorem b1 b2) -> validateTheorem (union (delete b2 a1) (delete a2 b1)) $ a2 =. b2
+  INST subs (Theorem assumes prop) -> validateTheorem (map (replace subs) assumes) (replace subs prop)
     where
     replace subs a = case a of
       Const _ _ -> a
@@ -89,15 +113,51 @@ inference rule = case rule of
 	Just t  -> t
       Abs v a -> Abs v $ replace [ (v', t) | (v', t) <- subs, v /= v' ] a
       Comb a b -> Comb (replace subs a) (replace subs b)
-  -- INST_TYPE           Theorem [(TypeVariable, TypeTerm)]
+  -- INST_TYPE           Theorem [(Type, Type)]
   -- AXIOM               Axiom
   _ -> Nothing
 
 -- | Validates (type checks) a theorem.
 validateTheorem :: [Term] -> Term -> Maybe Theorem
-validateTheorem assumptions proposition = Just $ Theorem assumptions proposition --XXX
+validateTheorem assumptions conclusion
+  | all (\ t -> wellTyped t && typeOf t == Bool) (conclusion : assumptions) = Just $ Theorem assumptions conclusion
+  | otherwise = Nothing
+
+-- | Checks if a term is well-typed.
+wellTyped :: Term -> Bool
+wellTyped a = wellTyped (freeVariables a) a
+  where
+  wellTyped :: [Variable] -> Term -> Bool
+  wellTyped env a = case a of
+    Const _ NotWellTyped -> False
+    Const _ _ -> True
+    Var v -> wellTypedVar env v
+    Comb fun arg -> case typeOf fun of
+      argType :-> _ -> typeOf arg == argType && wellTyped env fun && wellTyped env arg
+      _ -> False
+    Abs x t -> wellTyped (x : env) t
+
+  wellTypedVar :: [Variable] -> Variable -> Bool
+  wellTypedVar _ (Variable _ NotWellTyped) = False
+  wellTypedVar [] _ = False
+  wellTypedVar (Variable name t : rest) v@(Variable name' t')
+    | name == name' = t == t'
+    | otherwise = wellTypedVar rest v
 
 -- | Checks if a variable is free in a term.
 freeIn :: Variable -> Term -> Bool
-freeIn = undefined
+freeIn (Variable name _) t = elem name [ n | Variable n _ <- freeVariables t ]
+
+-- | All free variables in a term.
+freeVariables :: Term -> [Variable]
+freeVariables = nub . variables []
+  where
+  variables :: [Variable] -> Term -> [Variable]
+  variables env a = case a of
+    Const _ _ -> []
+    Var v
+      | elem v env -> []
+      | otherwise -> [v]
+    Comb a b -> variables env a ++ variables env b
+    Abs x a -> variables (x : env) a
 
